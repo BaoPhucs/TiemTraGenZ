06bf31469ae1a47f44f8595c37b84b1cdf79e61c
Linhvu0905 <linhvnpse182577@fpt.edu.vn>
2026-01-25 16:16:48 +0700

    add game menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Cần cái này để chuyển cảnh
+
+public class MainMenuController : MonoBehaviour
+{
+    // Hàm này sẽ gắn vào nút Play
+    public void PlayGame()
+    {
+        // Load cái màn chơi thành phố (Nhớ tên Scene phải chuẩn)
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Hàm này gắn vào nút Quit
+    public void QuitGame()
+    {
+        Debug.Log("Đã thoát game!");
+        Application.Quit();
+    }
+}
